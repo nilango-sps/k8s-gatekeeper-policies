23e9f235914daab20272da36fe62359341073243
Navin <nilango.sps@gmail.com>
2022-09-14 10:16:33 -0500

    first commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Installing Gatekeeper
+
+helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
+helm install gatekeeper/gatekeeper --name-template=gatekeeper --namespace gatekeeper-system --create-namespace
+kubectl get all --namespace gatekeeper-system
---
+++ scenarios.md
@@ -0,0 +1,7 @@
+Itr #1
+app-dashboard-1 preprod-app A
+app-dashboard-1 prod-app B
+
+Itr #2
+
+app-dashboard-1 prod-app A
